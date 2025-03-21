d59702212b0f3703c69e32eec2aa22d4ec81e4fb
Aditya122221 <116869022+Aditya122221@users.noreply.github.com>
2025-03-21 11:14:11 +0530

    Refactor database configuration to use DSN for MongoDB connection and comment out local connection settings

--- config/database.php
+++ config/database.php
@@ -30,13 +30,22 @@ return [
     */
 
     'connections' => [
+
+        // For loval DB Connection
+        // 'mongodb' => [
+        //     'driver'   => 'mongodb',
+        //     'host'     => env('DB_HOST', '127.0.0.1'),
+        //     'port'     => env('DB_PORT', 27017),
+        //     'database' => env('DB_DATABASE', 'TubeAcademyLaravel'),
+        //     'username' => env('DB_USERNAME', ''),
+        //     'password' => env('DB_PASSWORD', ''),
+        // ],
+
+        // For Online DB Connection
+
         'mongodb' => [
-            'driver'   => 'mongodb',
-            'host'     => env('DB_HOST', '127.0.0.1'),
-            'port'     => env('DB_PORT', 27017),
-            'database' => env('DB_DATABASE', 'TubeAcademyLaravel'),
-            'username' => env('DB_USERNAME', ''),
-            'password' => env('DB_PASSWORD', ''),
+            'driver' => 'mongodb',
+            'dsn' => env('DB_URI')
         ],
 
 
