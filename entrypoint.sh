2bcddca66fb76e0e2af718f2bbd3c149a3696fa3
Aditya122221 <adityakumar122221@gmail.com>
2025-04-01 18:32:09 +0530

    Deleted all files

--- entrypoint.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Default to port 8000 if PORT is not set
-PORT=${PORT:-8000}
-
-# Start Laravel's built-in server
-php artisan serve --host=0.0.0.0 --port=$PORT
--- resources/views/subheading.blade.php
+++
@@ -1,7 +0,0 @@
-<link rel="stylesheet" href="{{ asset('css/subheading.css') }}">
-<div class="container">
-    <a href="/student/nine" class="classin">Class IX</a>
-    <a href="/student/ten" class="classin">Class X</a>
-    <a href="/student/eleven" class="classin">Class XI</a>
-    <a href="/student/twelve" class="classin">Class XII</a>
-</div>
